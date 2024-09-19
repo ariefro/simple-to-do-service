32bf794db012adc1eb5982725cacc4b212508a29
arief romadhon <ariefromadhon26@gmail.com>
2024-09-19 09:32:19 +0700

    add read todo method

--- pkg/service/to-do-service.go
+++ pkg/service/to-do-service.go
@@ -3,11 +3,13 @@ package service
 import (
 	"context"
 	"database/sql"
+	"time"
 
 	todo "github.com/ariefro/simple-to-do-service/api/protogen/todos"
 	"github.com/ariefro/simple-to-do-service/pkg/util"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // toDoServiceServer is implementation of ToDoServiceServer proto interface
@@ -40,5 +42,38 @@ func (s *toDoServiceServer) Create(ctx context.Context, req *todo.CreateToDoRequ
 
 	return &todo.CreateToDoResponse{
 		Id: id,
+	}, nil
+}
+
+func (s *toDoServiceServer) Read(ctx context.Context, req *todo.ReadToDoRequest) (*todo.ReadToDoResponse, error) {
+	if req.GetId() == 0 {
+		return nil, status.Error(codes.InvalidArgument, "todo id is required")
+	}
+
+	query := "SELECT id, title, description, reminder FROM todo Where id = ?"
+
+	row := s.db.QueryRowContext(ctx, query, req.Id)
+
+	var id int64
+	var title, description string
+	var reminder time.Time
+
+	if err := row.Scan(&id, &title, &description, &reminder); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Error(codes.NotFound, "todo not found")
+		}
+
+		return nil, status.Error(codes.Internal, "failed to retrive todo: "+err.Error())
+	}
+
+	reminderProto := timestamppb.New(reminder)
+
+	return &todo.ReadToDoResponse{
+		ToDo: &todo.ToDo{
+			Id:          id,
+			Title:       title,
+			Description: description,
+			Reminder:    reminderProto,
+		},
 	}, nil
 }
